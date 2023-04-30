5dd16a93be567ccc3cb8cc7bdb5dece187d7eb85
Túlio <tuliofalmeida@hotmail.com>
2023-04-30 15:10:35 +0200

    0.2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.rst", "r") as fh:
 setuptools.setup(
     name = 'bacteria',         
     packages = ['bacteria'],   
-    version = '0.1.9',      
+    version = '0.2.0',      
     license='MIT',       
     description = 'Super Segger Analysis in Python.',
     long_description_content_type="text/x-rst",
